a3b3916b322ced0dd3c1959d4a59457587b7b3ae
Aleksandr Alpatkin <web_master.07@mail.ru>
2020-10-30 18:57:59 +0300

    add tests (selenium and spring) in market

---
+++ geekspring/shop/src/test/java/geekspring/market/SpringBootMockTests.java
@@ -0,0 +1,44 @@
+package geekspring.market;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SpringBootMockTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void tryToStart() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(org.hamcrest.core.StringContains.containsString("Добро пожаловать в интернет-магазин")));
+    }
+
+    @Test
+    public void checkQuantityProduct() throws Exception {
+        mockMvc.perform(get("/shop"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(xpath("/html/body/main/div/section/div/div[1]/div/div[2]/p").exists());
+    }
+
+}
